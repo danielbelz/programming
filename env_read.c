4d39f8d1aa1a3c9cf9277cb90422471ed9642c1f
danielbelz <47038500+danielbelz@users.noreply.github.com>
2019-01-25 11:16:53 -0800

    Adding env_read.c snippet
    Code to read environment variable from shell Linux

---
+++ env_read.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Read environment variable from shell
+
+int main()
+{
+    printf("Read PATH environment variable from shell\n");
+    
+    const char* s = getenv("PATH");
+    printf("PATH :%s\n",(s!=NULL)? s : "getenv returned NULL");
+    
+    return 0;
+}
